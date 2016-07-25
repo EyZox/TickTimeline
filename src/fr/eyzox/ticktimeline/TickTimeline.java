ff1b62095cf787e943a94c72bb44f1b1e9f6a4c7
Alexandre DUPONCHEL <alexandre.duponchel@gmail.com>
2016-07-25 19:04:51 +0200

    Fix generic bug

--- src/fr/eyzox/ticktimeline/TickTimeline.java
+++ src/fr/eyzox/ticktimeline/TickTimeline.java
@@ -1,7 +1,6 @@
 package fr.eyzox.ticktimeline;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
@@ -36,9 +35,9 @@ public class TickTimeline<DATA> {
 	 * D -> 7 ticks (A ticks + B ticks + C ticks + D ticks)
 	 * 
 	 */
-	public void add(final Collection<Node<? extends DATA>> dataCollection) {
+	public void add(final Collection<Node<DATA>> dataCollection) {
 		final ListIterator<Node<Collection<DATA>>> timelineIterator = timeline.listIterator();
-		for(final Node<? extends DATA> data : dataCollection) {
+		for(final Node<DATA> data : dataCollection) {
 			this.add(timelineIterator, data);
 		}
 	}
